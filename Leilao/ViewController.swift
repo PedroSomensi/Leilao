c487ca8f3110bf95fc84c649622f6e803783842c
Pedro Victor Somensi <psomensi.contato@gmail.com>
2023-08-11 00:22:53 -0300

    created initial project

---
+++ Leilao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Leilao
+//
+//  Created by Pedro Somensi on 11/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
